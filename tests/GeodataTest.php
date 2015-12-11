3c2031cd0d932e908aeed146013060e1b84a7299
Sam Henry <sam@given.to>
2015-12-11 13:26:30 -0500

    - Added new unit tests

--- tests/GeodataTest.php
+++ tests/GeodataTest.php
@@ -11,10 +11,38 @@ class GeodataTest extends PHPUnit_Framework_TestCase {
     $this->assertTrue(!empty($result));
   }
  
+  public function testGetCitiesLinks()
+  {
+    $geodata = new Geodata('json');
+	$result = $geodata->getCities('MD','links');
+    $this->assertTrue(!empty($result));
+  }
+ 
+  public function testGetCitiesPrimary()
+  {
+    $geodata = new Geodata('json');
+	$result = $geodata->getCities('MD','primary');
+    $this->assertTrue(!empty($result));
+  }
+ 
   public function testGetCounties()
   {
     $geodata = new Geodata('json');
 	$result = $geodata->getCounties('MD');
+    $this->assertTrue(!empty($result));
+  }
+ 
+  public function testGetCountiesLinks()
+  {
+    $geodata = new Geodata('json');
+	$result = $geodata->getCounties('MD','links');
+    $this->assertTrue(!empty($result));
+  }
+ 
+  public function testGetCountiesPrimary()
+  {
+    $geodata = new Geodata('json');
+	$result = $geodata->getCounties('MD','primary');
     $this->assertTrue(!empty($result));
   }
  
@@ -25,10 +53,38 @@ class GeodataTest extends PHPUnit_Framework_TestCase {
     $this->assertTrue(!empty($result));
   }
  
+  public function testGetCityLinks()
+  {
+    $geodata = new Geodata('json');
+	$result = $geodata->getCity('MD','Baltimore','links');
+    $this->assertTrue(!empty($result));
+  }
+ 
+  public function testGetCityPrimary()
+  {
+    $geodata = new Geodata('json');
+	$result = $geodata->getCity('MD','Baltimore','primary');
+    $this->assertTrue(!empty($result));
+  }
+ 
   public function testGetCountyData()
   {
     $geodata = new Geodata('json');
 	$result = $geodata->getCounty('MD','Montgomery County');
+    $this->assertTrue(!empty($result));
+  }
+ 
+  public function testGetCountyLinks()
+  {
+    $geodata = new Geodata('json');
+	$result = $geodata->getCounty('MD','Montgomery County','links');
+    $this->assertTrue(!empty($result));
+  }
+ 
+  public function testGetCountyPrimary()
+  {
+    $geodata = new Geodata('json');
+	$result = $geodata->getCounty('MD','Montgomery County','primary');
     $this->assertTrue(!empty($result));
   }
  
