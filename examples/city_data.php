1447718eea5a192b15fd5ef840de11568d100d83
Sam Henry <sam@gtwebsystems.com>
2015-12-12 12:15:47 -0500

    Applied fixes from StyleCI

--- examples/city_data.php
+++ examples/city_data.php
@@ -1,13 +1,14 @@
 <?php
 
 /**
- * SBA Geodata API
+ * SBA Geodata API.
  *
  * @link https://github.com/samhenry/sba-geodata-php
+ *
  * @author Sam Henry
+ *
  * @since 12.10.2015
  */
-
 require '../src/Geodata.php';
 use SamHenry\Geodata\Geodata;
 
@@ -15,19 +16,19 @@ use SamHenry\Geodata\Geodata;
 $geodata = new Geodata('json');
 
 // Retrieve all data for Baltimore, MD
-$result = $geodata->getCity('MD','Baltimore');
+$result = $geodata->getCity('MD', 'Baltimore');
 
 // Print Results
 echo '<pre>';
-foreach($result as $r){
-	echo 'Name: '.$r->name . PHP_EOL;
-	echo 'ID: '.$r->feature_id . PHP_EOL;
-	echo 'Class: '.$r->feat_class . PHP_EOL;
-	echo 'FIPS Class: '.$r->fips_class . PHP_EOL;
-	echo 'County: '.$r->full_county_name . PHP_EOL;
-	echo 'State: '.$r->state_name . PHP_EOL;
-	echo 'Coordinates: '.$r->primary_latitude.','.$r->primary_longitude. PHP_EOL;
-	echo 'URL: '.$r->url. PHP_EOL;
-	echo PHP_EOL;
+foreach ($result as $r) {
+    echo 'Name: '.$r->name.PHP_EOL;
+    echo 'ID: '.$r->feature_id.PHP_EOL;
+    echo 'Class: '.$r->feat_class.PHP_EOL;
+    echo 'FIPS Class: '.$r->fips_class.PHP_EOL;
+    echo 'County: '.$r->full_county_name.PHP_EOL;
+    echo 'State: '.$r->state_name.PHP_EOL;
+    echo 'Coordinates: '.$r->primary_latitude.','.$r->primary_longitude.PHP_EOL;
+    echo 'URL: '.$r->url.PHP_EOL;
+    echo PHP_EOL;
 }
 echo '</pre>';
